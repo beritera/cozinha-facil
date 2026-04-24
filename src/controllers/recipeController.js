4e0b015e80886c0f2815c6816491d8579efaf347
Bernardo Pierobon <berpierobon@gmail.com>
2026-04-24 00:49:49 -0300

    fix: corrige exportação das funções no recipeController

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -1,8 +1,28 @@
+const recipes = [];
+
+exports.getRecipes = (req, res) => {
+  res.json(recipes);
+};
+
+exports.createRecipe = (req, res) => {
+  const { title, description } = req.body;
+
+  const newRecipe = {
+    id: recipes.length + 1,
+    title,
+    description,
+  };
+
+  recipes.push(newRecipe);
+
+  res.status(201).json(newRecipe);
+};
+
 exports.updateRecipe = (req, res) => {
   const { id } = req.params;
   const { title, description } = req.body;
 
-  const recipe = recipes.find(r => r.id == id);
+  const recipe = recipes.find((recipe) => recipe.id == id);
 
   if (!recipe) {
     return res.status(404).json({ message: 'Receita não encontrada' });
@@ -17,7 +37,7 @@ exports.updateRecipe = (req, res) => {
 exports.deleteRecipe = (req, res) => {
   const { id } = req.params;
 
-  const index = recipes.findIndex(r => r.id == id);
+  const index = recipes.findIndex((recipe) => recipe.id == id);
 
   if (index === -1) {
     return res.status(404).json({ message: 'Receita não encontrada' });
