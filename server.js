38667d5280b0d79e9602d5b8ed1294da4f039993
Bernardo Pierobon <berpierobon@gmail.com>
2026-04-24 00:16:15 -0300

    refactor: integra routes ao server

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+const recipeRoutes = require('./src/routes/recipeRoutes');
+
+app.use(recipeRoutes);
+
 app.get('/', (req, res) => {
-  res.send('API Cozinha Fácil rodando 🚀');
+  res.send('API rodando 🚀');
 });
 
 app.listen(3000, () => {
-  console.log('Servidor rodando em http://localhost:3000');
+  console.log('Servidor rodando');
 });
