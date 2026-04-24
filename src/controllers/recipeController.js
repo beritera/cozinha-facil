dd3c2372d8d2de602ef01ee16e3f63432f9206dd
Bernardo Pierobon <berpierobon@gmail.com>
2026-04-24 00:38:03 -0300

    feat: adiciona armazenamento em memória para receitas

--- src/controllers/recipeController.js
+++ src/controllers/recipeController.js
@@ -1,3 +1,19 @@
+const recipes = [];
+
 exports.getRecipes = (req, res) => {
-  res.send('Lista de receitas');
+  res.json(recipes);
+};
+
+exports.createRecipe = (req, res) => {
+  const { title, description } = req.body;
+
+  const newRecipe = {
+    id: recipes.length + 1,
+    title,
+    description,
+  };
+
+  recipes.push(newRecipe);
+
+  res.status(201).json(newRecipe);
 };
