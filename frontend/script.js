4d4c4f2a999e46ccb752fa51cdacab7c1e603b5a
Bernardo Pierobon <berpierobon@gmail.com>
2026-04-25 21:53:39 -0300

    fix: corrige erro de digitação no código

--- frontend/script.js
+++ frontend/script.js
@@ -21,9 +21,13 @@ async function loadRecipes() {
       <h3>${recipe.title}</h3>
       <p>${recipe.description || 'Sem descrição'}</p>
 
-      <button onclick="editRecipe(${recipe.id}, '${recipe.title}', '${recipe.description || ''}')"> Editar </button>
+      <button onclick="editRecipe(${recipe.id}, '${recipe.title}', '${recipe.description || ''}')">
+        Editar
+      </button>
 
-      <button class="delete-btn" onclick="deleteRecipe(${recipe.id})">Excluir</button>
+      <button class="delete-btn" onclick="deleteRecipe(${recipe.id})">
+        Excluir
+      </button>
     `;
 
     recipesList.appendChild(card);
@@ -33,52 +37,49 @@ async function loadRecipes() {
 form.addEventListener('submit', async (event) => {
   event.preventDefault();
 
-const recipeData = {
+  const recipeData = {
     title: titleInput.value,
     description: descriptionInput.value,
-};
+  };
 
-if (editingRecipeId) {
+  if (editingRecipeId) {
     await fetch(`${API_URL}/${editingRecipeId}`, {
-        method = 'PUT',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(recipeData),
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(recipeData),
     });
 
     editingRecipeId = null;
-
-}   else {
+  } else {
     await fetch(API_URL, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(recipeData),
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(recipeData),
     });
-}
+  }
 
-    titleInput.value = '';
-    descriptionInput.value = '';
+  titleInput.value = '';
+  descriptionInput.value = '';
 
-    loadRecipes();
+  loadRecipes();
 });
 
-    function editRecipe(id, title, description) {
-        editRecipeId = id,
-        titleInput.value = title,
-        descriptionInput.value = description;
-    }
+function editRecipe(id, title, description) {
+  editingRecipeId = id;
+  titleInput.value = title;
+  descriptionInput.value = description;
+}
 
-    async function deleteRecipe(id) {
-        await fetch(`${API_URL}/${id}`, {
-            method: 'DELETE',
-        });
+async function deleteRecipe(id) {
+  await fetch(`${API_URL}/${id}`, {
+    method: 'DELETE',
+  });
 
-        loadRecipes();
+  loadRecipes();
+}
 
-    }
-
-    loadRecipes();
-  
+loadRecipes();
