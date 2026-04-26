288088fb12a3fe0b0030c1a1ce59ec16f6fdbe6b
Bernardo Pierobon <berpierobon@gmail.com>
2026-04-25 21:34:14 -0300

    feat: habilita CORS no backend

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const recipeRoutes = require('./src/routes/recipeRoutes');
 
+app.use(cors());
 app.use(express.json());
 app.use(recipeRoutes);
 
